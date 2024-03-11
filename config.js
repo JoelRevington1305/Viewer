b0e899a8bf86cb32aee0616a090cc1e1e1cda29c
JoelRevington1305 <joelrevington28@gmail.com>
2024-03-11 12:46:19 +0530

    build fix

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
 let { APS_CLIENT_ID, APS_CLIENT_SECRET, APS_BUCKET, PORT } = process.env;
-// if (!APS_CLIENT_ID || !APS_CLIENT_SECRET) {
-//     console.warn('Missing some of the environment variables.');
-//     process.exit(1);
-// }
-// APS_BUCKET = APS_BUCKET || `${APS_CLIENT_ID.toLowerCase()}-basic-app`;
+if (!APS_CLIENT_ID || !APS_CLIENT_SECRET) {
+    console.warn('Missing some of the environment variables.');
+    process.exit(1);
+}
+APS_BUCKET = APS_BUCKET || `${APS_CLIENT_ID.toLowerCase()}-basic-app`;
 PORT = PORT || 8080;
 
 module.exports = {
