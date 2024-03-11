fd788a7af7ebd5d3f10756f820806fbfaaa1156c
JoelRevington1305 <joelrevington28@gmail.com>
2024-03-11 17:57:52 +0530

    api token call check

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const { getPublicToken } = require('../services/aps.js');
 let router = express.Router();
 
 router.get('/api/auth/token', async function (req, res, next) {
+    console.log("api token call")
     try {
         res.json(await getPublicToken());
     } catch (err) {
