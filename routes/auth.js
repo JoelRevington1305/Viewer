e38574b09f220d4286136e4412f7e2afe9f614d0
JoelRevington1305 <joelrevington28@gmail.com>
2024-03-11 15:13:57 +0530

    check

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ router.use(cors({
 router.get('/api/auth/token', async function (req, res, next) {
     try {
         const publicToken = await getPublicToken();
-        res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+        res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
         res.json(publicToken);
     } catch (err) {
