00c0bade82c018c738206c879c122b865581b706
JoelRevington1305 <joelrevington28@gmail.com>
2024-03-11 15:02:03 +0530

    added cors

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const { getPublicToken } = require('../services/aps.js');
 
 let router = express.Router();
-
+router.use(cors())
 router.get('/api/auth/token', async function (req, res, next) {
     try {
         res.json(await getPublicToken());
